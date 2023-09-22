9024cfd5a120fda7ff0d55d9f14812a1f347db81
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 10:49:45 -0400

    npm cleanup

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
+import { FsApiModule } from '@firestitch/api';
 import { FsExampleModule } from '@firestitch/example';
 import { FsMessageModule } from '@firestitch/message';
-import { FsApiModule } from '@firestitch/api';
 import { FsProgressModule } from '@firestitch/progress';
 
-import { ToastrModule } from 'ngx-toastr';
-
-import { AppMaterialModule } from './material.module';
-import { ExampleComponent, ExamplesComponent } from './components';
 import { AppComponent } from './app.component';
+import { ExampleComponent, ExamplesComponent } from './components';
+import { AppMaterialModule } from './material.module';
 
 const routes: Routes = [
   { path: '', component: ExamplesComponent },
 ];
 
+
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     FsApiModule.forRoot(),
@@ -30,7 +29,6 @@ const routes: Routes = [
     FormsModule,
     FsExampleModule.forRoot(),
     FsMessageModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
     RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
   ],
   entryComponents: [
