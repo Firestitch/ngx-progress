d2bca5303027461afe4aacc835703a53991acfb8
Raymond Gigliotti <ray@firestitch.com>
2024-02-20 17:24:24 -0500

    Angular 13 Upgrade

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -20,27 +20,24 @@ const routes: Routes = [
 
 
 @NgModule({
-  bootstrap: [AppComponent],
-  imports: [
-    BrowserModule,
-    FsApiModule.forRoot(),
-    FsProgressModule.forRoot({ barColor: 'pink' }),
-    BrowserAnimationsModule,
-    AppMaterialModule,
-    FormsModule,
-    FsExampleModule.forRoot(),
-    FsMessageModule.forRoot(),
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
-  ],
-  entryComponents: [
-  ],
-  declarations: [
-    AppComponent,
-    ExamplesComponent,
-    ExampleComponent,
-  ],
-  providers: [
-  ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        FsApiModule.forRoot(),
+        FsProgressModule.forRoot({ barColor: 'pink' }),
+        BrowserAnimationsModule,
+        AppMaterialModule,
+        FormsModule,
+        FsExampleModule.forRoot(),
+        FsMessageModule.forRoot(),
+        RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    ],
+    declarations: [
+        AppComponent,
+        ExamplesComponent,
+        ExampleComponent,
+    ],
+    providers: []
 })
 export class PlaygroundModule {
 }
